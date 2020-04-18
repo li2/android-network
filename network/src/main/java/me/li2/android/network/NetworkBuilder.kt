0d6d86a34973b01792dc5b1434efcda494fc0ffd
Weiyi Li <weiyi.just2@gmail.com>
2020-04-19 00:21:44 +1200

    Updated RequestInterceptor to intercept url, e.g. add request parameters globally. Updated network builder with debug option.

--- network/src/main/java/me/li2/android/network/NetworkBuilder.kt
+++ network/src/main/java/me/li2/android/network/NetworkBuilder.kt
@@ -3,20 +3,23 @@
  * https://github.com/li2
  */
 @file:Suppress("unused")
+
 package me.li2.android.network
 
 import android.content.Context
+import com.facebook.stetho.okhttp3.StethoInterceptor
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.readystatesoftware.chuck.ChuckInterceptor
 import com.serjltt.moshi.adapters.Wrapped
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.serialization.json.Json
-import me.li2.android.network.interceptor.DefaultRequestInterceptor
-import me.li2.android.network.interceptor.RequestInterceptor
-import me.li2.android.network.interceptor.ResponseInterceptor
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level.BODY
 import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -28,18 +31,25 @@ object NetworkBuilder {
     inline fun <reified T> buildRetrofitAdapter(
             context: Context,
             baseUrl: String,
-            responseInterceptor: ResponseInterceptor,
-            requestInterceptor: RequestInterceptor = DefaultRequestInterceptor(),
+            interceptors: List<Interceptor> = emptyList(),
             converterFactory: Converter.Factory = moshiConverterFactory,
-            timeout: Long = TIMEOUT): T {
+            timeout: Long = TIMEOUT,
+            debug: Boolean = false): T {
         val okHttpClient = OkHttpClient.Builder()
-                    .addInterceptor(requestInterceptor)
-                    .addInterceptor(responseInterceptor)
-                    .addNetworkInterceptor(context)
-                    .readTimeout(timeout, TimeUnit.SECONDS)
-                    .connectTimeout(timeout, TimeUnit.SECONDS)
-                    .writeTimeout(timeout, TimeUnit.SECONDS)
-                    .build()
+                .apply {
+                    interceptors.forEach { interceptor ->
+                        addInterceptor(interceptor)
+                    }
+                    if (debug) {
+                        addNetworkInterceptor(StethoInterceptor())
+                        addNetworkInterceptor(HttpLoggingInterceptor().setLevel(BODY))
+                        addInterceptor(ChuckInterceptor(context))
+                    }
+                }
+                .readTimeout(timeout, TimeUnit.SECONDS)
+                .connectTimeout(timeout, TimeUnit.SECONDS)
+                .writeTimeout(timeout, TimeUnit.SECONDS)
+                .build()
 
         return Retrofit.Builder()
                 .baseUrl(baseUrl)
--- network/src/main/java/me/li2/android/network/interceptor/RequestInterceptor.kt
+++ network/src/main/java/me/li2/android/network/interceptor/RequestInterceptor.kt
@@ -4,20 +4,30 @@
  */
 package me.li2.android.network.interceptor
 
+import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 
 abstract class RequestInterceptor : Interceptor {
 
+    abstract var urlBuilder: (HttpUrl.Builder.() -> Unit)
+
+    abstract var requestBuilder: (Request.Builder.() -> Unit)
+
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request()
-        val newRequest = request.newBuilder().run {
-            interceptRequest(this)
+        val originalRequest = chain.request()
+
+        val newUrl = originalRequest.url.newBuilder().run {
+            urlBuilder.invoke(this)
+            build()
+        }
+
+        val newRequest = originalRequest.newBuilder().run {
+            requestBuilder.invoke(this)
+            url(newUrl)
             build()
         }
         return chain.proceed(newRequest)
     }
-
-    abstract fun interceptRequest(requestBuilder: Request.Builder)
 }
